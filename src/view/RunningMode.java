394ca4b21e14167ace4e80943b892fb0b61fb2ff
ninwhippa08 <ypolatoglu20@ku.edu.tr>
2024-04-26 17:42:01 +0300

    Running mode frame opened

---
+++ src/view/RunningMode.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class RunningMode extends JFrame {
+    
+    public RunningMode(){
+        setTitle("Play Game");
+        setSize(1920,1080);
+    }
+}
