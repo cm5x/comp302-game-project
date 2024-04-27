7d46bddd362bb5ba600321b1150b7481fa61a6a4
emredumanli <edumanli20@ku.edu.tr>
2024-04-27 23:50:11 +0300

    updated barrier

--- src/view/main.java
+++ src/view/main.java
@@ -3,7 +3,7 @@ package view;
 public class main {
     public static void main(String[] args) {
         //test the frame
-        settingsPage st = new settingsPage();
+        test t = new test();
 
     }
 }
