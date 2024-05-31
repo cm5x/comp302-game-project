0937708c9c4bc5e0098017dcbe8cd9d079a899e1
cm5x <91802400+cm5x@users.noreply.github.com>
2024-05-31 23:26:18 +0300

    Update Server.java

--- src/gameMechanics/Server.java
+++ src/gameMechanics/Server.java
@@ -12,6 +12,12 @@ public class Server extends Thread {
     private ServerSocket serverSocket;
     private Socket socket;
 
+    private int hostScore = 0;
+    private int clientScore = 0;
+    private DataOutputStream hostOut;
+    private DataOutputStream clientOut;
+    private RunningMode runningMode;
+
     public Server(int port) throws IOException {
         serverSocket = new ServerSocket(port);
         serverSocket.setSoTimeout(50000); // No timeout for waiting for the client
@@ -25,6 +31,9 @@ public class Server extends Thread {
             System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "...");
             socket = serverSocket.accept();
             System.out.println("Client connected: " + socket.getRemoteSocketAddress());
+
+            hostOut = new DataOutputStream(socket.getOutputStream());
+
 
             // // Close the waiting screen if it's open
             // MultiplayerWaitingScreen waitingScreen = MultiplayerWaitingScreen.getInstance();
@@ -49,7 +58,7 @@ public class Server extends Thread {
 
         // Start the game for the host
         Player player = new Player("uname", "pass");
-        RunningMode runningMode = new RunningMode(1, player, initialMap, null);
+        RunningMode runningMode = new RunningMode(1, player);
         runningMode.setVisible(true);
     }
 
@@ -63,6 +72,36 @@ public class Server extends Thread {
             }
         }
     }
+
+    public void updateScore(boolean isHost, int score) {
+        try {
+            if (isHost) {
+                hostScore = score;
+            } else {
+                clientScore = score;
+            }
+            broadcastScores();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void broadcastScores() throws IOException {
+        if (hostOut != null) {
+            hostOut.writeInt(hostScore);
+            hostOut.writeInt(clientScore);
+        }
+        if (clientOut != null) {
+            clientOut.writeInt(hostScore);
+            clientOut.writeInt(clientScore);
+        }
+    }
+
+    public void setClientOut(DataOutputStream clientOut) {
+        this.clientOut = clientOut;
+    }
+
+
 
     private ArrayList<int[]> generateInitialMap() {
         // Generate initial map for the game
