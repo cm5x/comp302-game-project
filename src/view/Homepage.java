c6f69f19934f3d4dc0fbe68222151b5e85c3ebd8
cm5x <91802400+cm5x@users.noreply.github.com>
2024-05-31 20:53:21 +0300

    Update Homepage.java

--- src/view/Homepage.java
+++ src/view/Homepage.java
@@ -42,10 +42,9 @@ public class Homepage extends JFrame {
         JButton playButton = new JButton("Play Game");
         playButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                MapSlotsFrame run = new MapSlotsFrame();
+                PlayGame run = new PlayGame();
                 run.p.username = username;
                 run.setVisible(true);
-                closeHome();
             }
         });
         panel.add(playButton);
